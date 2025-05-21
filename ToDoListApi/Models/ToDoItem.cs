a3032123931b5af038c6874c963efbb7ba77767d
SpectroXRL <spectrothedev@gmail.com>
2025-05-21 18:33:18 -0500

    Created ToDoItem Model

---
+++ ToDoListApi/Models/ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApi.Models
+{
+    public class ToDoItem
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Title { get; set; }
+
+        [Required]
+        public bool IsDone { get; set; }
+
+        public DateOnly? DueDate { get; set; }
+    }
+}
